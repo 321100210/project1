44697ad1dbe53d373f30624f6e062748a3453be1
321100210 <152067167+321100210@users.noreply.github.com>
2023-11-27 20:03:30 +0200

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'currency_converter_screen.dart';
+
+void main() => runApp(CurrencyConverterApp());
+
+class CurrencyConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Currency Converter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CurrencyConverter(),
+    );
+  }
+}
